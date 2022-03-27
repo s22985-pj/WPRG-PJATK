688f3b31a48259a7db7c7ef4af3d35dc2777c350
s22985-pj <s22985@pjwstk.edu.pl>
2022-03-27 15:47:10 +0200

    Zad1 - Calculator

--- Z2/Zad1/script.php
+++ Z2/Zad1/script.php
@@ -1,1 +1,23 @@
 <?php
+
+
+$l1 = $_POST['licz1'];
+$l2 = $_POST['licz2'];
+$oprnd = $_POST['choice'];
+echo "$l1<br>";
+echo "$l2<br>";
+echo "$oprnd<br>";
+
+if($oprnd=="+")
+    $wynik=$l1+$l2;
+
+else if($oprnd=="-")
+    $wynik=$l1-$l2;
+
+else if($oprnd=="*")
+    $wynik=$l1*$l2;
+
+else if($oprnd=="/")
+    $wynik=$l1/$l2;
+
+echo $wynik;
