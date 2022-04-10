74e689c4ea783b3b8953513d95c6d51faf92989c
s22985-pj <s22985@pjwstk.edu.pl>
2022-04-10 13:25:57 +0200

    Zad2 - Form

---
+++ Z2/Zad2/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+    <h1>Rezerwacja pokoju</h1>
+
+    <form action="script.php" method="post">
+        <h4>
+            Liczba osób<br>
+            <input name="guest_quantity" type="number" value="1" min="1" max="4"><br>
+        </h4>
+        <h4>
+            Dane rezerwującego<br>
+            <input name="name" type="text" placeholder="Imię"><br>
+            <input name="surname" type="text" placeholder="Nazwisko"><br>
+        </h4>
+        <h4>
+            Adres rezerwującego <br>
+            <input name="street" type="text" placeholder="Ulica"><input name="street_num" type="number" placeholder="Nr domu">
+            <br><input name="town" type="text" placeholder="Miejscowość">
+            <br><input name="post_code" placeholder="Kod pocztowy">
+        </h4>
+        <h4>
+
+            <input name="mail" type="email" placeholder="Adres e-mail">
+        </h4>
+            Data pobytu <br>
+            od: <input name="date_from" type="date">
+            do: <input name="date_to" type="date">
+        <h4>
+            Dane karty płatniczej <br>
+            <input name="card_number" type="number"  minlength="16" maxlength="16" placeholder="16-cyfrowy numer karty"><br>
+            <input name="date_card" type="text" placeholder="data ważności karty"><br>
+            <input name="CVV" type="number" minlength="3" maxlength="3" placeholder="Numer cvv" size="1">
+        </h4>
+        <h4>
+            Dodatkowe udogodnienia <br>
+            <h5>Łóżeczko dla dziecka:
+                <input name="kid_bed" type="checkbox"><br></h5>
+            <h5>Klimatyzacja:
+                <input name="clima" type="checkbox"><br></h5>
+            <h5>Popielniczna:
+                <input name="ashtray" type="checkbox"><br></h5>
+
+            <input type="submit" value="Dalej">
+        </h4>
+    </form>
+
+</body>
+</html>
